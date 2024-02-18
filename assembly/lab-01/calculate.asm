6134d03c5b993b03eb9da42f9877c7027748c45d
Artem F. <81350694+jus1d@users.noreply.github.com>
2024-02-18 10:08:35 +0000

    upd: ./assembly/lab-01

--- assembly/lab-01/calculate.asm
+++ assembly/lab-01/calculate.asm
@@ -1,12 +1,30 @@
 global calculate
 
+section .data
+    zero_div_message db "Can't divide by zero. Use other values.", 10
+
 section .text
 calculate:
+    ; -- a,    b,    c,    d --
     ; -- arg0, arg1, arg2, arg3 --
-    ; -- rdi,  rsi,  rdx,  r10  -- 
-    ; -- (-2 * a + 6 - 3 * b)/(-2 * c * c - 55 / d) -- 
-    mov rax, rdi
-    add rax, rsi
-    add rax, rdx
-    add rax, rcx
+    ; -- rdi,  rsi,  rdx,  rcx  -- 
+
+    mov rax, -55
+    mov rbx, rcx
+    cdq
+    idiv rbx
+
+
     ret
+    
+
+error_zero_div:
+    mov     rax, 1
+    mov     rdi, 2
+    mov     rsi, zero_div_message
+    mov     rdx, 40
+    syscall
+
+    mov     rax, 60
+    mov     rdi, 1
+    syscall
--- assembly/lab-01/main.cpp
+++ assembly/lab-01/main.cpp
@@ -2,19 +2,23 @@
 #include <locale>
 #include "./calculate.h"
 
+using namespace std;
+
 int main() {
     setlocale(LC_ALL, "Russian");
 
     int a, b, c, d;
-    std::cout << "Введите a, b, c, d: " << std::endl;
-    std::cin >> a >> b >> c >> d;
+    cout << "Введите a, b, c, d: " << endl;
+    // cin >> a >> b >> c >> d;
+    a = 1;
+    b = -1;
+    c = 0;
+    d = 5;
 
     int result = calculate(a, b, c, d);
 
-    // a = 1, b = 1, c = 
-    // (1)/(-2 * c * c - 55 / d)
-
-    printf("Выражение:\n(-2 * a + 6 - 3 * b)/(-2 * c * c - 55 / d)\n");
+    printf("Выражение:\n(-2 * a + 6 - 3 * b)/(-2 * c * c - 55 / d)\n"); // = 7, if a = 1, b = -1, c = 0, d = -55 // 1 -1 0 -55
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
     printf("Ожидаемый результат:    %d\n", ((-2 * a) + 6 - (3 * b)) / ((-2 * c * c) - (55 / d)));
     printf("Полученный результат:   %d\n", result);
 

f5a68e1c5b14418cdf76ed6ccc53a7dc67b4ee6d
Artem F. <81350694+jus1d@users.noreply.github.com>
2024-03-03 18:59:41 +0000

    Update asm/lab-03

--- assembly/lab-03/main.cpp
+++ assembly/lab-03/main.cpp
@@ -10,7 +10,8 @@ int calculate_cpp(vector<int> vec, size_t vec_sz, int d)
     int counter = 0;
     for (size_t i = 0; i < vec.size(); ++i)
     {
-        if (vec[i] < 0 && vec[i] <= d) counter++;
+        // if (vec[i] < 0 && vec[i] <= d) counter++;
+        if (vec[i] < 0) counter++;
     }
     return counter;
 }
@@ -18,7 +19,7 @@ int calculate_cpp(vector<int> vec, size_t vec_sz, int d)
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    vector<int> vec = {1, -2, -1, -4, 5};
+    vector<int> vec = {-1, 2, -1, 4, -5};
     int d = -2;
 
     cout << "[C++] Количество отрицательных элементов массива, которые удовлетворяют условию: a[i] <= d: " << calculate_cpp(vec, vec.size(), d) << endl;
